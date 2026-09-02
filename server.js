a36399fae7ddbc0010dd41d2627db6728432d387
impactmindz <impactmindztech@gmail.com>
2026-09-02 12:19:48 +0530

    changes

--- server.js
+++ server.js
@@ -10,3 +10,4 @@ const startServer = async() => {
 }
 
 startServer();
+    
--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
-
+import dns from 'dns';
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 const connectDB = async() => {
     try{
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Database connected successfully");
     }catch(err){
+        console.log(process.env.MONGO_URI)
+
         console.log("Error connecting to the database:", err);
         process.exit(1);
     }
